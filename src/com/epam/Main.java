2041e5ff7ff63620c0d56d4aceed58ab8c1500d9
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-03-08 22:02:09 +0200

    Initial commit

---
+++ src/com/epam/Main.java
@@ -0,0 +1,38 @@
+package com.epam;
+
+import java.util.StringTokenizer;
+
+/**
+ * Created by hrinchenko on 08.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+        String str = "This is String, split by StringTokenizer. Created by Student:Name of Student";
+for(int i =0; i<=2; i++){
+    if(i==0){
+        StringTokenizer s1 = new StringTokenizer(str,",");
+        while (s1.hasMoreElements()){
+            System.out.println(s1.nextElement());
+
+        }
+        System.out.println("***********************");
+    }else if(i==1){
+        StringTokenizer s1 = new StringTokenizer(str," ");
+        while (s1.hasMoreElements()){
+            System.out.println(s1.nextElement());
+
+        }
+        System.out.println("********************");
+    }else if(i==2){
+
+        StringTokenizer s1 = new StringTokenizer(str,".");
+        while(s1.hasMoreElements()){
+            System.out.println(s1.nextElement());
+        }
+    }
+
+}
+    }
+}
